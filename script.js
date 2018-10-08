cb7852f7bb66fa968ae5a1310d4d719f8457e54c
mariumtech2000 <38943256+mariumtech2000@users.noreply.github.com>
2018-10-08 17:48:05 +0500

    test-2

---
+++ script.js
@@ -0,0 +1,16 @@
+function msg(){
+    alert("message send");
+}
+
+function run1(){
+    document.querySelector("input").innerHTML ="A";
+}
+function run2(){
+    document.querySelector("input").innerHTML ="B";
+}
+function run3(){
+    document.querySelector("input").innerHTML ="C";
+}
+function run4(){
+    document.querySelector("input").innerHTML ="D";
+}
